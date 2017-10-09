e3701daef8f4d8daa248dd86b14b7602d580c7b8
Ilan Pillemer <ilan.pillemer@gmail.com>
2017-10-09 22:26:07 +0100

    got tests working

--- src/main/kotlin/hello.kt
+++ src/main/kotlin/hello.kt
@@ -2,4 +2,22 @@ fun main(args: Array<String>) {
     println("Hello World!")
 }
 
+fun triangles() {
+    for (c in 1..40) {
+        for (b in 1..c) {
+            for (a in 1..b){
+                if (a*a + b*b == c*c) {
+                    println("$a,$b,$c")
+                } 
+            }
+        }
+    }
+}
+
+// fun anagrams(a : String, n : Int ) {
+//     if (n == 0) {
+
+//     }
+// }
+
 
---
+++ src/test/kotlin/tests.kt
@@ -0,0 +1,22 @@
+import kotlin.test.assertEquals
+import org.junit.Test as test
+
+class TestSource() {
+    @test fun f() {
+        // val example : KotlinGreetingJoiner = KotlinGreetingJoiner(Greeter("Hi"))
+        // example.addName("Harry")
+        // example.addName("Ron")
+        // example.addName(null)
+        // example.addName("Hermione")
+        assertEquals(1 + 1, 2)
+    }
+
+    @test fun test_triangles() {
+        triangles()
+    }
+
+    @test fun `Heaps Agorithm`() {
+        anagrams("rat",2)
+    }
+
+}
